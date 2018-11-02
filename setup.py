39f60c2f16a721e7d791e7edc3f6a5a6179f74ce
gecrooks <gec@threeplusone.com>
2018-11-02 12:52:40 -0700

    Update setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 """QuantumFlow setup"""
 
 from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name='quantumflow',
@@ -22,4 +27,24 @@ setup(
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     package_data={'': ['datasets/data/*.*']},
-)
+
+    description="QuantumFlow: A Quantum Algorithms Development Toolkit",
+    long_description=long_description,
+    license='Apache-2.0',
+    maintainer="Gavin Crooks",
+    maintainer_email="gavin@rigetti.com",
+    url="https://github.com/rigetticomputing/quantumflow/",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Quantum Computing',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+        ]
+    )
